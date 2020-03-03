2b392da8e22604f520ea304ba3cad25c8bb790e0
Nalexx6 <60155114+Nalexx6@users.noreply.github.com>
2020-03-03 13:19:03 +0200

    Labs 1(data base), 2a(stack realization), 2b.1(unrolled linked list)

---
+++ cmake-build-debug/CMakeFiles/Lab_2b_1_Unrolled_linked_list_.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "D:/C++/1/1 course 2 term(2020)/Lab_2b.1(Unrolled linked list)/Functions.cpp" "D:/C++/1/1 course 2 term(2020)/Lab_2b.1(Unrolled linked list)/cmake-build-debug/CMakeFiles/Lab_2b_1_Unrolled_linked_list_.dir/Functions.cpp.obj"
+  "D:/C++/1/1 course 2 term(2020)/Lab_2b.1(Unrolled linked list)/main.cpp" "D:/C++/1/1 course 2 term(2020)/Lab_2b.1(Unrolled linked list)/cmake-build-debug/CMakeFiles/Lab_2b_1_Unrolled_linked_list_.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/Lab_2b_1_Unrolled_linked_list_.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Lab_2b_1_Unrolled_linked_list_.dir/Functions.cpp.obj"
+  "CMakeFiles/Lab_2b_1_Unrolled_linked_list_.dir/main.cpp.obj"
+  "Lab_2b_1_Unrolled_linked_list_.exe"
+  "Lab_2b_1_Unrolled_linked_list_.exe.manifest"
+  "Lab_2b_1_Unrolled_linked_list_.pdb"
+  "libLab_2b_1_Unrolled_linked_list_.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Lab_2b_1_Unrolled_linked_list_.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
